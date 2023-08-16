a2267a1c765b8e4b0fda63d6aa827fb2e89dc564
nhuihomestay <114401327+nhuihomestay@users.noreply.github.com>
2023-08-16 20:26:20 +0700

    first implement

---
+++ fetch_test.ts
@@ -0,0 +1,23 @@
+fetch("https://api.debank.com/article/repost", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "th-TH,th;q=0.9,en;q=0.8,sn;q=0.7",
+    "account": "{\"random_at\":1686945307,\"random_id\":\"6496ee24d0d44e61b4be31868a694e58\",\"session_id\":\"38700ebae3f94b3d9a3a85a709a3f4fe\",\"user_addr\":\"0xd4562d7d62cefebada19449a0ebd4a8d2afd0976\",\"wallet_type\":\"metamask\",\"is_verified\":true}",
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "source": "web",
+    "x-api-nonce": "n_GEzcTfuigS2kpNhSiqqvThqJebvn8ioCQ5Nv008E",
+    "x-api-sign": "69a9bae7f426827c1707344d367211522dae8095ac8bc01a01508d85d1b08de3",
+    "x-api-ts": "1692184990",
+    "x-api-ver": "v2",
+    "Referer": "https://debank.com/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "{\"id\":441518}",
+  "method": "POST"
+});
